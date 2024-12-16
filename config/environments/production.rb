979c773b3df9411a04f261f5f918636a3e742936
Wahid Sholihul Fikri <wahidsf1905@gmail.com>
2024-12-16 21:31:48 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.active_support.to_time_preserves_timezone = :zone
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
